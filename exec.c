c539a41cb3424e40f77e3ee736d59c813fcfedba
clz <13303657947@163.com>
2025-06-08 18:49:39 +0800

    fork

--- exec.c
+++ exec.c
@@ -2,7 +2,7 @@
 #include "user/user.h"
 
 int main(){
-    char *argv[] = ("echo","this","is","echo",0);
+    char *argv[] = {"echo","this","is","echo",0};
     exec("echo",argv);
     printf("exec failed\n");
     exit(0);
